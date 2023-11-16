837b6565a42022e8d2418d785d957044820c0f99
Kaushik Subramaniam Gnanaskandan <kgnanask@amazon.com>
2023-11-16 00:18:32 -0800

    Add amplify preset

--- apps/website1/nuxt.config.ts
+++ apps/website1/nuxt.config.ts
@@ -1,4 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+	nitro: {
+		// preset: provider === 'aws_amplify' ? "./amplify" : undefined,
+		preset:  "./amplify"
+	},
 	modules: ['nuxt3-websites-package']
 })
